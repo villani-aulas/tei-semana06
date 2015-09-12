a266a70accce21ba24e2687175eae787ec79c196
LeonardoVillani <informatica@villani.eti.br>
2015-09-12 10:39:09 -0300

    Definição da página de inicial e da página de contato do projeto.

---
+++ contato.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8" />
+	<title>Página de Contato</title>
+</head>
+<body>
+	<h1>Página de Contato</h1>
+	<a href="index.php">Voltar</a>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8" />
+	<title>Página inicial do projeto</title>
+</head>
+<body>
+	<h1>Página inicial do projeto</h1>
+	<a href="contato.php">Ir para contato</a>
+</body>
+</html>
